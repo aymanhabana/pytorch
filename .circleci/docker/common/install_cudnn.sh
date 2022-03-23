05cdeab1051afeec668fe8e8a378b397a75827fe
atalman <atalman@fb.com>
2022-03-23 13:05:45 -0700

    cudnn testing

--- .circleci/docker/common/install_cudnn.sh
+++ .circleci/docker/common/install_cudnn.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
 if [[ ${CUDNN_VERSION} == 8 ]]; then
-
-    sudo apt-get update
-    # also install ssh to avoid error of:
-    # --------------------------------------------------------------------------
-    # The value of the MCA parameter "plm_rsh_agent" was set to a path
-    # that could not be found:
-    #   plm_rsh_agent: ssh : rsh
-    sudo apt-get install -y ssh
-    sudo apt-get update && apt-get install -y --no-install-recommends libcudnn8=8.3.2.44 libcudnn8-dev=8.3.2.44 && apt-mark hold libcudnn8
-
     # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
-    #mkdir tmp_cudnn && cd tmp_cudnn
-    #CUDNN_NAME="cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive"
-    #curl -OLs  https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/${CUDNN_NAME}.tar.xz
-    #tar xf ${CUDNN_NAME}.tar.xz
-    #cp -a ${CUDNN_NAME}/include/* /usr/local/cuda/include/
-    #cp -a ${CUDNN_NAME}/lib/* /usr/local/cuda/lib64/
-    #cd ..
-    #rm -rf tmp_cudnn
-    #ldconfig
+    mkdir tmp_cudnn && cd tmp_cudnn
+    CUDNN_NAME="cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive"
+    curl -OLs  https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/${CUDNN_NAME}.tar.xz
+    tar xf ${CUDNN_NAME}.tar.xz
+    cp -a ${CUDNN_NAME}/include/* /usr/local/cuda/include/
+    cp -a ${CUDNN_NAME}/lib/* /usr/local/cuda/lib64/
+    chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
+    cd ..
+    rm -rf tmp_cudnn
+    ldconfig
 fi
